231d11296c42dbc314665b9c5e54271f32cfd25f
zhanyihong <zhangyh@epb360.com>
2017-10-20 09:09:21 +0800

    账号密码格式校验

--- src/logic/impl/IAppImpl.js
+++ src/logic/impl/IAppImpl.js
@@ -1,4 +1,6 @@
+import RNValidate from '../../util/RNValidate.js';
 
+//获取图片
 export async function fetchImage(page = parseInt(190 * Math.random())) {
     let url = 'http://i.jandan.net/?oxwlxojflwblxbsapi=jandan.get_ooxx_comments&page=' + page
     const result = await fetch(url);
@@ -6,49 +8,58 @@ export async function fetchImage(page = parseInt(190 * Math.random())) {
     PPZ.cache.imageList = res.comments
 }
 
+//注册
 export async function register(params, fn) {
     return new Promise(async (resolve, reject) => {
-        const res = await fetch(global.PPZ.host + '/account/register', {
-            method: 'POST',
-            headers: {
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(params)
-        })
-        const Data2Json = await res.json();
-        console.log('res----->', Data2Json)
-        if (Data2Json.code === 1) {
-            //1.成功
-            const _onLogin = () => {
-                global.modal.close();
-                PPZ.router.resetTo('welcome')(fn);
+        try {
+            await RNValidate.match(params.mobile, 'mobile', '手机号格式不正确');
+            await RNValidate.match(params.password, 'password', '密码格式不正确');
+            const res = await fetch(global.PPZ.host + '/account/register', {
+                method: 'POST',
+                headers: {
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(params)
+            })
+            const Data2Json = await res.json();
+            console.log('res----->', Data2Json)
+            if (Data2Json.code === 1) {
+                //1.成功
+                const _onLogin = () => {
+                    global.modal.close();
+                    PPZ.router.resetTo('welcome')(fn);
+                }
+                const _onClose = () => {
+                    global.modal.close();
+                }
+                const data = {
+                    tips: Data2Json.tip,
+                    buttons: [
+                        { title: '登录', cb: _onLogin },
+                        { title: '取消', cb: _onClose },
+                    ]
+                }
+                global.modal.open('RNAlert', data);
+                resolve(Data2Json)
             }
-            const _onClose = () => {
-                global.modal.close();
+            else {
+                const tip = Data2Json.tip
+                // await global.modal.close();
+                //2.失败
+                //2.1打开模态提示 失败
+                global.modal.open('RNAlert', { tips: tip });
+                return;
             }
-            const data = {
-                tips: Data2Json.tip,
-                buttons: [
-                    { title: '登录', cb: _onLogin },
-                    { title: '取消', cb: _onClose },
-                ]
-            }
-            global.modal.open('RNAlert', data);
-            resolve(Data2Json)
-        }
-        else {
-            const tip = Data2Json.tip
-            // await global.modal.close();
-            //2.失败
-            //2.1打开模态提示 失败
-            global.modal.open('RNAlert', { tips: tip });
-            return;
+
+        } catch (error) {
+            global.modal.open('RNAlert', { tips: error });
         }
     })
 }
 
+//登录
 export async function login(params, fn) {
     return new Promise(async (resolve, reject) => {
         const res = await fetch(global.PPZ.host + '/account/login', {
@@ -74,21 +85,28 @@ export async function login(params, fn) {
     })
 }
 
+//重置密码
 export async function reserpsd(params) {
-    const res = await fetch(global.PPZ.host + '/account/resetpsd', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(params)
+    try {
+        await RNValidate.match(params.mobile, 'mobile', '手机号格式不正确');
+        await RNValidate.match(params.password, 'password', '密码格式不正确');
+        const res = await fetch(global.PPZ.host + '/account/resetpsd', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(params)
 
-    })
-    const data = await res.json();
-    console.log('res------>', data)
-    const tip = data.tip
-    global.modal.open('RNAlert', { tips: tip });
-    return;
+        })
+        const data = await res.json();
+        console.log('res------>', data)
+        const tip = data.tip
+        global.modal.open('RNAlert', { tips: tip });
+        return;
+    } catch (error) {
+        global.modal.open('RNAlert', { tips: error });
+    }
 }
 
 
